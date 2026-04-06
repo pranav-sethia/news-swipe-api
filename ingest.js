b17c898d4bad370cd8450ec00d0112467ef29ce8
Pranav Sethia <psethia@uci.edu>
2026-04-06 16:42:31 -0700

    feat: switch ingest.js to fetch fresh breaking news by date instead of static front page

--- ingest.js
+++ ingest.js
@@ -10,7 +10,7 @@ const { pipeline } = require('@xenova/transformers');
 // Config
 // ---------------------------------------------------------------------------
 const HN_ALGOLIA_URL =
-  'https://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=100';
+  'https://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=points>=10&hitsPerPage=80';
 
 // Minimum points a story must have to be ingested (filters out low-quality posts)
 const MIN_POINTS = 10;
