98bacf2aab05c1ccdc0d8c92346aa895a4631afd
Pranav Sethia <psethia@uci.edu>
2026-06-24 20:09:21 +0800

    chore: clean up redundant scrape scripts and migrate ingest.js to use Groq API for cloud automation

--- test_scrape.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-axios.get('https://animationobsessive.substack.com/p/the-image-boards-of-hayao-miyazaki')
-  .then(() => console.log('Success!'))
-  .catch(err => console.log('Failed:', err.response ? err.response.status : err.message));
